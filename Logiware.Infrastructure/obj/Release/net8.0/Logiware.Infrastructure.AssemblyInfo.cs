a6f6c3dd041d6367b158dbe30bc83227d153b5ef
miko <canaremiko3@gmail.com>
2025-02-03 00:43:41 +0800

    reroute

--- Logiware.Infrastructure/obj/Release/net8.0/Logiware.Infrastructure.AssemblyInfo.cs
+++ Logiware.Infrastructure/obj/Release/net8.0/Logiware.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logiware.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6469d5b0d915968ac0fe4813eafbf30a132bc282")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logiware.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logiware.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Logiware.Test/obj/Release/net8.0/Logiware.Test.AssemblyInfo.cs
+++ Logiware.Test/obj/Release/net8.0/Logiware.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logiware.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6469d5b0d915968ac0fe4813eafbf30a132bc282")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logiware.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logiware.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
